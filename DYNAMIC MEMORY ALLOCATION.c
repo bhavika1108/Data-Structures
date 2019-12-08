539c51705da9930de4566aab6292e56485256233
bhavika1108 <43167486+bhavika1108@users.noreply.github.com>
2019-12-08 23:20:07 +0530

    Update DYNAMIC MEMORY ALLOCATION.c

--- DYNAMIC MEMORY ALLOCATION.c
+++ DYNAMIC MEMORY ALLOCATION.c
@@ -1,21 +1,25 @@
-#include&lt;stdio.h&gt;
-#include&lt;string.h&gt;
-#include&lt;process.h&gt;
+#include<stdio.h>
+#include<string.h>
+#include<process.h>
 
 int main()
 {
+
 int n[10],i;
 int *p,size;
-printf(&quot;Enter size of array : \n&quot;);
-scanf(&quot;%d&quot;,&amp;size);
+
+printf("Enter size of array : \n");
+scanf("%d",&size);
+
 p=(int*)malloc(size*sizeof(int));
-printf(&quot;Enter Array : \n&quot;);
-for(i=0;i&lt;size;i++)
+
+printf("Enter Array : \n");
+for(i=0;i<size;i++)
 {
-scanf(&quot;%d&quot;,p+i);
+	scanf("%d",p+i);
 }
-for(i=0;i&lt;size;i++)
+for(i=0;i<size;i++)
 {
-printf(&quot;%d&quot;,*(p+i));
+	printf("%d",*(p+i));
 }
 }
